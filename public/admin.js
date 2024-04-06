5073910c51f6dd2874376c115615ff85977267d2
valeriasolorza <v.solorza.343@gmail.comcd>
2024-04-06 12:13:06 -0700

    Started Code in Admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,17 @@
 
 // Your Code Here
+let response = await fetch('http://localhost:9001/updateBook', {
+    method: "PATCH",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        "title": "Legends of Arathrae"
+    })
+});
+
+if (!response.ok) {
+    throw new Error('Failed to update book title');
+}
+
+console.log('Book title updated successfully');
